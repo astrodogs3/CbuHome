4a26d5c6d92afb63f97626baff71a07bb0965310
Ron <rsmccary@juno.com>
2012-05-30 22:24:51 -0700

    change controllers application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+layout "cbustore"
+  
+  before_filter :authorize, :except => :login
+  
+  helper :all # include all helpers, all the time
+  
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery :secret => '8fc080370e56e929a2d5afca5540a0f7'
+
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password").
+  # Scrub sensitive parameters from your log
+  # filter_parameter_logging :password
+
+  protected
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      flash[:notice] = "Please log in"
+      redirect_to :controller => 'admin', :action => 'login'
+    end
+  end
 end
